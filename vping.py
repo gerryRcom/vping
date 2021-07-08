6f1605175c13d9ee7c93ac29c2a015875226c50f
gerryRcom <gerryr@gerryr.com>
2021-07-08 23:11:02 +0100

    Track previous results and initial text colour highlighting

--- vping.py
+++ vping.py
@@ -26,18 +26,22 @@ def pingItem(pinginput):
 def main():
   if (os.path.isfile('ping.txt')):
     ip4Regex = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
+    os.system('clear')
     print("################################")
     print("## Preparing initial results ...")
 
+    # need to declare arrays here to allow comparison with previous info
+    resolving = []
+    resolvingTo = []
+    notResolving = []    
+    bcolorsTag = [] 
     i = 0
     while (i < 5):
 
       with open('ping.txt') as items:
         allItems = items.readlines()
         itemsCount = len(allItems)
-        resolving = []
-        resolvingTo = []
-        notResolving = []      
+ 
 
         #print("%s" % allItems[itemsCount-1])
 
@@ -55,18 +59,34 @@ def main():
                   resolvingTo.append(cleanWord)
                   notResolving.append("empty")
 
+                  if (i > 0):
+                    print(resolvingTo[(i * itemsCount)-itemsCount])
+                    print(cleanWord)
+                    if (resolvingTo[(i * itemsCount)-itemsCount] == cleanWord):
+                      bcolorsTag.append("\033[93m")
+                    else:
+                      bcolorsTag.append("\033[92m")
+                  else:
+                    bcolorsTag.append("\033[93m")
+
             elif ("not known" in pingResultLine):
               #print("## %s not resolving " %item.rstrip())
+              # append to all arrays to keep index in sync
               notResolving.append(item.rstrip())
               resolving.append("empty")
               resolvingTo.append("empty")
+              bcolorsTag.append("\033[93m")
         
-        os.system('clear')
+        # clear screen each time just before printing latest info
+        #os.system('clear')
+        # set default colour for the list of VMs to amber
+        bcolors.vmTag='\033[93m'
         print("################################")
         print("##     Pinging %s devices " %str(itemsCount))
-        for x in range(len(resolving)):
+        # only print the last results info
+        for x in range(i*itemsCount, len(resolving)):
           if (not "empty" in resolving[x]):
-            print("## %s resolving to: %s %s" %(resolving[x], resolvingTo[x], x))
+            print("## %s resolving to: %s %s %s %s" %(resolving[x], bcolorsTag[x], resolvingTo[x], bcolors.END, x))
           else:
             print("## %s is not resolving %s " %(notResolving[x], i))
       
